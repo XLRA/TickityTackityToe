8b2dbcb9e1634de30da9512bdedcf2b588801c02
XLRA <90275060+XLRA@users.noreply.github.com>
2023-01-14 02:18:28 -0500

    Source code upload

---
+++ TickTackToe/SwiftView.swift
@@ -0,0 +1,41 @@
+//
+//  SquareView.swift
+//  TickityTacTow
+//
+//  Created by XLRA (Venkat Yenduri on 1/13/2023
+//
+import Foundation
+import SwiftUI
+
+enum SquareStatus {
+    case empty
+    case home
+    case visitor
+}
+
+class Square : ObservableObject {
+    @Published var squareStatus : SquareStatus
+    
+    init(status : SquareStatus) {
+        self.squareStatus = status
+    }
+}
+
+struct SquareView : View {
+    @ObservedObject var dataSource : Square
+    var action: () -> Void
+    var body: some View {
+        Button(action: {
+            self.action()
+        }, label: {
+            Text(self.dataSource.squareStatus == .home ?
+                    "X" : self.dataSource.squareStatus == .visitor ? "0" : " ")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.black)
+                .frame(width: 70, height: 70, alignment: .center)
+                .background(Color.gray.opacity(0.3).cornerRadius(10))
+                .padding(4)
+        })
+    }
+}
---
+++ TickTackToe/TickTackToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTackToeApp.swift
+//  TickTackToe
+//
+//  Created by Venkat Yenduri on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TickTackToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
